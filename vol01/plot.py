bc4011102104afd74a330a09f23bd145affee8f1
Hajime Fujimoto <hajime2008x@yahoo.co.jp>
2020-12-22 13:46:01 +0000

    Vol01 first commit

---
+++ vol01/plot.py
@@ -0,0 +1,13 @@
+﻿import matplotlib.pyplot as plt
+
+plt.figure(dpi = 120, figsize = (16, 9))
+x = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+y = [ 5.6, 7.2, 10.6, 13.6, 20.0, 21.8, 24.1, 28.4, 25.1, 19.4, 13.1, 8.5 ]
+plt.plot(x, y, label = "Average temperature")
+plt.grid()
+plt.legend()
+plt.xlabel("Month")
+plt.ylabel("Temperature")
+plt.title("Changes in average temperature in Tokyo")
+plt.savefig("graph.png", format = "png")
+plt.show()
